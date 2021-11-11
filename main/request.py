dc5abfba08624d34f05a340a31a8ee70453b2bda
Joshua inyang <joshuainyang255@gmail.com>
2021-11-11 19:47:10 +0100

    Handling exception errors

--- main/request.py
+++ main/request.py
@@ -1,6 +1,42 @@
-import requests 
+import requests
+from requests.exceptions import RequestException
+from requests.sessions import TooManyRedirects, session 
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 
-# re = requests.get("")
+Webpage_url = "https://www.coingecko.com/it/monete/recently_added"
+coin_url = 'https://api.coinxgecko.com/api/v3/coins/list'
 
-# data = re['']
+
+
+def get_coin(web, api):
+    request_timeout = 120
+
+    session = requests.session()
+    retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[502, 503, 504])
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+
+    try:
+
+        Webpage_url_response = session.get(web)
+        coin_url_response = session.get(api, timeout=request_timeout).json()
+
+
+
+        for line in Webpage_url_response.text.splitlines():
+            if '<td class="py-0 coin-name" data-sort=' in line:
+                name = line[len('<td class="py-0 coin-name" data-sort=')+1:-2]
+                print(f"This name of the coin is: {name}")   ## Displays the name of the new coin
+
+            for coin in coin_url_response:
+                if coin['name'] == name:
+                    new_coin = requests.get("https://api.coinxgecko.com/api/v3/coins/" + coin['id'])
+                    print(new_coin.json())   ## Displays the details of the newly listed coin from coingecko in the command line
+
+    # Check for posiible exception errors
+    except (ConnectionError, TimeoutError, TooManyRedirects) as e:
+        print(e)
+                
+
+get_coin(Webpage_url, coin_url)
